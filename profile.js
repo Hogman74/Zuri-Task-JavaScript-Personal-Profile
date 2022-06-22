c0d011dd2421c693c0b3acfbe53ca15ef90fd798
Ikechukwu Onunkwo <hogman74life@gmail.com>
2022-06-23 00:41:11 +0100

    initial commit

---
+++ profile.js
@@ -0,0 +1,11 @@
+var name
+name = prompt("Enter Your Name: ");
+alert("Your name is " + name);
+
+var height
+height = prompt("Enter Your Height: ");
+alert("Your height is " + height);
+
+var countryOrigin
+countryOrigin = prompt("Enter Your Country Of Origin: ");
+alert("Your country of origin is " + countryOrigin);
